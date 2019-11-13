a47d3a90b9cf451866d8868a450728959958c009
Emmanuel <jamestame787@gmail.com>
2019-11-12 21:41:48 -0500

    1st commit
    Created the initial project

---
+++ Web Application Project/Sample/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Models;
+
+namespace Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult DataPage()
+        {
+            return View();
+        }
+ 
+        public IActionResult SettingsPage()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserPage()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View(new CustomerDetails());
+        }
+
+        [HttpPost]
+        public IActionResult Insert(CustomerDetails obj)
+        {
+            FakeRepository.AddNewCustomer(obj);
+
+            return View("DisplayPage", FakeRepository.GetCustomer());
+        }
+        public IActionResult ItemsList()
+        {
+            return View(FakeRepository.GetCustomer());
+        }
+        public IActionResult DisplayPage()
+        {
+            return View(FakeRepository.GetCustomer());
+        }
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Web Application Project/Sample/Models/CustomerDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Models
+{
+    public class CustomerDetails
+    {
+        public int PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Please enter a Firstname")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a Lastname")]
+        public string Lastname { get; set; }
+        [Required(ErrorMessage = "Please Budget")]
+        public int Amount { get; set; }
+
+    }
+}
