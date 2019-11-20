547578b5bc0afb289dbbfca1687267b6481545d2
Emmanuel <jamestame787@gmail.com>
2019-11-19 23:52:10 -0500

    4th commit

---
+++ COMP229-Estore-added cart component/COMP229-Estore-master/COMP229-Estore-master/Web Application Project/Sample/Models/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Models
+{
+    public class ProductRepository
+    {
+        public static List<Product> storeProducts = new List<Product>();
+
+        public static IEnumerable<Product> Products
+        {
+            get
+            {
+                return storeProducts;
+            }
+        }
+
+        public static void AddProductToStore(Product product)
+        {
+            storeProducts.Add(product);
+        }
+
+        public static void AddProductsToStore(IEnumerable<Product> products)
+        {
+            storeProducts.AddRange(products);
+        }
+    }
+}
