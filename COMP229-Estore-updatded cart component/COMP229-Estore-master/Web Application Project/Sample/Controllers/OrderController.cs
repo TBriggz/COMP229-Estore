66edb337913f15aa7946f0095ca5cb3a66df3fe2
Emmanuel <jamestame787@gmail.com>
2019-11-20 21:11:46 -0500

    6th commit

---
+++ COMP229-Estore-updatded cart component/COMP229-Estore-master/Web Application Project/Sample/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Models;
+
+namespace Sample.Controllers
+{
+    public class OrderController : Controller
+    {
+        private Cart cart;
+
+        
+
+       
+ 
+        public OrderController(Cart aCart)
+        {
+            cart = aCart;
+        }
+        public ViewResult DataPage()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View("Cart", RefreshCart.UserCart);
+        }
+       
+
+        public RedirectToActionResult AddToCart(int productId)
+        {
+            //Product product = ProductRepository.storeProducts
+            //.FirstOrDefault(p => p.ProductID == productId);
+            //if (product != null)
+            //{
+            //    RefreshCart.Refresh(product);
+            //}
+            for(int aProduct = 0; aProduct < ProductRepository.storeProducts.Count; aProduct++)
+            {
+                if(ProductRepository.storeProducts[aProduct].ProductID == productId)
+                {
+                    RefreshCart.Refresh(ProductRepository.storeProducts[aProduct]);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public RedirectToActionResult RemoveFromCart(int productId)
+        {
+            for (int aProduct = 0; aProduct < ProductRepository.storeProducts.Count; aProduct++)
+            {
+                if (ProductRepository.storeProducts[aProduct].ProductID == productId)
+                {
+                    RefreshCart.Delete(ProductRepository.storeProducts[aProduct]);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ViewResult Checkout()
+        {
+            return View();
+        }
+
+    }
+}
